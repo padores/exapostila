40d50558a078a22288d3d2589d414b67eed2ea81
Pedrosa <pedrosa@ubuntu.ubuntu-domain>
2013-09-18 20:54:21 -0300

    Teste de ambos os métodos

--- testContas.java
+++ testContas.java
@@ -2,18 +2,57 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-
 public class testContas {
 
 	Contas C1 = new Contas();
-	
+
 	@Test
-	public void testCalculaQuadrado() {
+	public void testCalculaQuadrado1() {
 		assertEquals(0, C1.calculaQuadrado(0));
-		assertEquals(1, C1.calculaQuadrado(1));
-		assertEquals(1, C1.calculaQuadrado(-1));
+	}
+
+	@Test
+	public void testCalculaQuadrado2() {
+		assertEquals(4, C1.calculaQuadrado(2));
+	}
+
+	@Test
+	public void testCalculaQuadrado3() {
+		assertEquals(4, C1.calculaQuadrado(-2));
+	}
+
+	@Test
+	public void testCalculaQuadrado4() {
 		assertEquals(10000, C1.calculaQuadrado(100));
+	}
+
+	@Test
+	public void testCalculaQuadrado5() {
 		assertEquals(10000, C1.calculaQuadrado(-100));
 	}
 
+	@Test
+	public void testCalculaCubo1() {
+		assertEquals(0, C1.calculaCubo(0));
+	}
+
+	@Test
+	public void testCalculaCubo2() {
+		assertEquals(8, C1.calculaCubo(2));
+	}
+
+	@Test
+	public void testCalculaCubo3() {
+		assertEquals(-8, C1.calculaCubo(-2));
+	}
+
+	@Test
+	public void testCalculaCubo4() {
+		assertEquals(1000000, C1.calculaCubo(100));
+	}
+
+	@Test
+	public void testCalculaCubo5() {
+		assertEquals(-1000000, C1.calculaCubo(-100));
+	}
 }
